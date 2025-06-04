05de67d35c26bc08370f29333966dba51d149422
DjCr4y0n <83771077+DjCr4y0n@users.noreply.github.com>
2025-06-04 10:34:22 +0200

    knn with more columns from csv
    Added SibSP, Parch, Fare and Embarked

--- main.py
+++ main.py
@@ -33,14 +33,17 @@ class KNearestNeighbors:
 df_train = pd.read_csv('train.csv')
 
 df_train['Sex'] = df_train['Sex'].map({'female': 0, 'male': 1})
+df_train['Embarked'] = df_train['Embarked'].map({'C': 0, 'Q': 1, 'S': 2})
 
-X = df_train[['Pclass', 'Sex', 'Age', 'Fare']]  # features
+X = df_train[['Pclass', 'Sex', 'Age','SibSp','Parch', 'Fare', 'Embarked']]  # features
 y = df_train['Survived']                        # labels
 
 
-# uzupełnianie medianą brakującego wieku
+# uzupełnianie medianą brakującego danych
 X.loc[:, 'Age'] = X['Age'].fillna(X['Age'].median())
 X.loc[:, 'Fare'] = X['Fare'].fillna(X['Fare'].median())
+X.loc[:, 'Parch'] = X['Parch'].fillna(X['Parch'].median())
+X.loc[:, 'SibSp'] = X['SibSp'].fillna(X['SibSp'].median())
 
 # dzielnie danych na testowe i treningowe
 X_train, X_test, y_train, y_test = train_test_split(
@@ -56,7 +59,7 @@ for features, label in zip(X_train, y_train):
     titanic_data[label].append(features)
 
 #użycie metody knn
-clf = KNearestNeighbors(k=10)
+clf = KNearestNeighbors(k=3)
 clf.fit(titanic_data)
 
 # sprawdzenie ile predykcji udało się zrobić
@@ -70,47 +73,54 @@ for features, actual in zip(X_test, y_test):
 
 #sprawdzenie dokładności
 accuracy = correct / len(y_test)
-#print(f"Accuracy: {accuracy:.2f}")
+print(f"Accuracy: {accuracy:.2f}")
 
 #confusion matrix
 confusion_matrix = metrics.confusion_matrix(predicted_list, y_test)
 cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = [0, 1])
 cm_display.plot()
-#plt.show()
+plt.show()
 
 
+def PredictTestData():
+    #pobranie testowych danych
+    df_test = pd.read_csv("test.csv")
+    df_test = df_test[['PassengerId','Pclass','Sex', 'Age','SibSp','Parch', 'Fare', 'Embarked']]
+    df_test['Sex'] = df_test['Sex'].map({'female': 0, 'male': 1})
+    df_test['Embarked'] = df_test['Embarked'].map({'C': 0, 'Q': 1, 'S': 2})
+
+    df_test.loc[:, 'Age'] = df_test['Age'].fillna(X['Age'].median())
+    df_test.loc[:, 'Fare'] = df_test['Fare'].fillna(X['Fare'].median())
+    df_test.loc[:, 'Parch'] = df_test['Parch'].fillna(X['Parch'].median())
+    df_test.loc[:, 'SibSp'] = df_test['SibSp'].fillna(X['SibSp'].median())
 
 
-#pobranie testowych danych
-df_test = pd.read_csv("test.csv")
-df_test = df_test[['PassengerId','Pclass', 'Sex', 'Age', 'Fare']]
-df_test['Sex'] = df_test['Sex'].map({'female': 0, 'male': 1})
+    #uzupełnienie NaN
+    df_test = df_test.fillna(df_test.median(numeric_only=True))
 
-#uzupełnienie NaN
-df_test = df_test.fillna(df_test.median(numeric_only=True))
+    test_data = df_test.values.tolist()
 
-test_data = df_test.values.tolist()
+    #predykcja danych
+    test_predictions = []
+    for row in test_data:
+        passenger_id = row[0]         # First element
+        features = row[1:]            # Remaining values for prediction
 
-#predykcja danych
-test_predictions = []
-for row in test_data:
-    passenger_id = row[0]         # First element
-    features = row[1:]            # Remaining values for prediction
+        predicted = clf.predict(features)
+        test_predictions.append([passenger_id, predicted])
 
-    predicted = clf.predict(features)
-    test_predictions.append([passenger_id, predicted])
 
-print(test_predictions)
+    #sformatowanie danych pod wysyłke do kaggle
+    output_data = {'PassengerId':[], 'Survived':[]}
+    for data in test_predictions:
+        output_data['PassengerId'].append(int(data[0]))
+        output_data['Survived'].append(int(data[1]))
 
-#sformatowanie danych pod wysyłke do kaggle
-output_data = {'PassengerId':[], 'Survived':[]}
-for data in test_predictions:
-    output_data['PassengerId'].append(int(data[0]))
-    output_data['Survived'].append(int(data[1]))
+    return pd.DataFrame(output_data)
 
-df = pd.DataFrame(output_data)
-print(df)
-df.to_csv('output.csv', index=False)
+
+#print(PredictTestData)
+#PredictTestData.to_csv('output.csv', index=False)
 
 
 
