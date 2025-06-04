14f2d4615f83f9e900ca4457841fef0f3b1e2216
DjCr4y0n <83771077+DjCr4y0n@users.noreply.github.com>
2025-06-04 09:54:32 +0200

    ready and exported data
    I used here KNN, but with very little data, only few columns

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from collections import Counter
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
 
 
 #Deklaracja metody knn
@@ -26,41 +29,90 @@ class KNearestNeighbors:
         result = Counter(categories).most_common(1)[0][0]
         return result
 
+#pobranie danych z csv
 df_train = pd.read_csv('train.csv')
-df_train = df_train[['Survived', 'Pclass', 'Sex', 'Age', 'Fare']]  # wybierz kolumny
 
 df_train['Sex'] = df_train['Sex'].map({'female': 0, 'male': 1})
 
+X = df_train[['Pclass', 'Sex', 'Age', 'Fare']]  # features
+y = df_train['Survived']                        # labels
+
+
+# uzupełnianie medianą brakującego wieku
+X.loc[:, 'Age'] = X['Age'].fillna(X['Age'].median())
+X.loc[:, 'Fare'] = X['Fare'].fillna(X['Fare'].median())
+
+# dzielnie danych na testowe i treningowe
+X_train, X_test, y_train, y_test = train_test_split(
+    X.values.tolist(), y.tolist(), test_size=0.2, random_state=42
+)
+
+#wrzucanie wszystko do jednego dict
 titanic_data = {}
 
-for _, row in df_train.iterrows():
-    label = row['Survived']
-    features = [row['Pclass'], row['Sex'], row['Age'], row['Fare']]
-
+for features, label in zip(X_train, y_train):
     if label not in titanic_data:
         titanic_data[label] = []
     titanic_data[label].append(features)
 
-#odpalanie KNN
-clf  = KNearestNeighbors()
+#użycie metody knn
+clf = KNearestNeighbors(k=10)
 clf.fit(titanic_data)
+
+# sprawdzenie ile predykcji udało się zrobić
+predicted_list = []
+correct = 0
+for features, actual in zip(X_test, y_test):
+    predicted = clf.predict(features)
+    predicted_list.append(predicted)
+    if predicted == actual:
+        correct += 1
+
+#sprawdzenie dokładności
+accuracy = correct / len(y_test)
+#print(f"Accuracy: {accuracy:.2f}")
+
+#confusion matrix
+confusion_matrix = metrics.confusion_matrix(predicted_list, y_test)
+cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = [0, 1])
+cm_display.plot()
+#plt.show()
+
+
+
+
 
 df_test = pd.read_csv("test.csv")
 df_test = df_test[['PassengerId','Pclass', 'Sex', 'Age', 'Fare']]
+df_test['Sex'] = df_test['Sex'].map({'female': 0, 'male': 1})
 
-df_test = df_test['Sex'] = df_test['Sex'].map({'female': 0, 'male': 1})
 
-test_data = {}
+df_test = df_test.fillna(df_test.median(numeric_only=True))
 
-for _, row in df_test.iterrows():
-    label = row['PassengerId']
-    features = [row['Pclass'], row['Sex'], row['Age'], row['Fare']]
+test_data = df_test.values.tolist()
 
-    if label not in test_data:
-        test_data[label] = []
-    test_data[label].append(features)
+test_predictions = []
+for row in test_data:
+    passenger_id = row[0]         # First element
+    features = row[1:]            # Remaining values for prediction
 
-print(test_data)
+    predicted = clf.predict(features)
+    test_predictions.append([passenger_id, predicted])
+
+print(test_predictions)
+
+output_data = {'PassengerId':[], 'Survived':[]}
+for data in test_predictions:
+    output_data['PassengerId'].append(int(data[0]))
+    output_data['Survived'].append(int(data[1]))
+
+df = pd.DataFrame(output_data)
+print(df)
+df.to_csv('output.csv', index=False)
+
+
+
+
 
 
 
